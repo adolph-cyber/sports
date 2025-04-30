1deb0fcfa5e780afd0d5391589657e6bc43dd85f
adolph-cyber <adolphkatsomolefe@gmail.com>
2025-04-30 07:42:07 +0200

    Delete TeamException.java

--- TeamException.java
+++
@@ -1,9 +0,0 @@
-public class TeamException extends Exception{
-    public TeamException(String message){
-        super(message);
-    }
-
-    public TeamException(String message, Throwable cause){
-        super(message, cause);
-    }
-}
